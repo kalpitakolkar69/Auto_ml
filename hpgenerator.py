4651c4c95f22c47c2d65b33793fcb7fe4b09bf8d
kalpitakolkar69 <kalpit.akolkar69@gmail.com>
2020-06-09 21:45:33 +0530

    second commit

---
+++ hpgenerator.py
@@ -0,0 +1,46 @@
+# Creates json file on basis of hyper parameters 
+import json
+
+
+def dense_tweak(top_dense, n_dense):
+    return [int(top_dense / pow(2, i)) for i in range(n_dense)]
+
+
+input_shape = (32, 32, 3)
+
+no_of_conv = [i for i in range(1, 5)]
+no_of_dense_layer = [i for i in range(2, 6)]
+top_dense_layer = [256, 512, 1024]
+pool_krn = 2
+lr = 0.001
+bs = 128
+
+a = []
+for n_convo in no_of_conv:
+    for n_dense in no_of_dense_layer:
+        for k in top_dense_layer:
+            convo_krn = [11, 9, 7, 5, 3]
+            n_filter = [32 * pow(2, i) for i in range(n_convo)]
+            neu_fstd = dense_tweak(k, n_dense)
+            hyper_params = {'n_convo': n_convo,
+                            'n_dense': n_dense,
+                            'convo_krn': convo_krn,
+                            'n_filter': n_filter,
+                            'pool_krn': pool_krn,
+                            'top_dense_layer': k,
+                            'neu_fstd': neu_fstd,
+                            'lr': lr,
+                            'bs': bs}
+            a.append(hyper_params)
+
+
+b = {'hyper_params': a}
+print(b)
+
+with open('model_hparam.json', 'w') as f:
+    json.dump(b, f, indent=2)
+
+c = {'result': []}
+with open('results.json', 'w') as g:
+    json.dump(c, g, indent=2)
+   
---
+++ tweak.py
@@ -0,0 +1,28 @@
+from docker import DockerClient
+from json import load
+from os import system
+from smtpserver import email_msg
+from time import sleep
+
+# Connects to docker service over tcp
+client = DockerClient("tcp://192.168.0.102:4040")
+
+# Runs docker container with first configerations
+system('sudo docker run -itd --rm -v /home/kalpit/Projectfiles/Mlopstask3:/home akolkarkalpit/mytensorflow:v1 python -c 0 /home/main.py')
+
+
+while True:
+    if not client.containers.list():
+        with open('results.json', 'r') as f:
+            data = load(f)
+            temp = data['result'][-1]
+            val_accuracy = temp['val_accuracy'][-1]
+            model = temp['model']
+        if val_accuracy < 0.8:
+            system('sudo docker run -itd --rm -v /home/kalpit/Projectfiles/Mlopstask3:/home akolkarkalpit/mytensorflow:v1 python -c {} /home/main.py'.format(model+1))
+        elif val_accuracy > 0.8:
+            email_msg('Your model {} has achived 80% efficiency'.format(model))
+            break
+    sleep(60)
+
+print('Crongratulations !!')
